fd4f50807e0888ac57bcc5f98a6e83e8d5102555
David Barina <ibarina@fit.vutbr.cz>
2019-01-31 17:25:44 +0100

    First commit

---
+++ bf.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+
+FILE *pstream;
+unsigned char mem[0x10000];
+unsigned char *ptr = mem;
+
+long skip()
+{
+	long pos = ftell(pstream);
+
+	while (1) {
+		switch (fgetc(pstream)) {
+			case '[':
+				skip();
+				break;
+			case ']':
+				return pos;
+			case EOF:
+				return pos;
+			default:
+				;
+		}
+	}
+}
+
+void call();
+
+long exec()
+{
+	long pos = ftell(pstream);
+
+	while (1) {
+		switch (fgetc(pstream)) {
+			case '>':
+				++ptr;
+				break;
+			case '<':
+				--ptr;
+				break;
+			case '+':
+				++*ptr;
+				break;
+			case '-':
+				--*ptr;
+				break;
+			case '.':
+				putchar(*ptr);
+				break;
+			case ',':
+				*ptr = getchar();
+				break;
+			case '[':
+				if (*ptr) {
+					call();
+				} else {
+					skip();
+				}
+				break;
+			case ']':
+				return pos;
+			case EOF:
+				return pos;
+			default:
+				;
+		}
+	}
+}
+
+void call()
+{
+	fseek(pstream, exec()-1, SEEK_SET);
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc - 1) {
+		pstream = fopen(argv[1], "r");
+	}
+
+	if (NULL == pstream) {
+		pstream = stdin;
+	}
+
+	exec();
+
+	if (stdin != pstream) {
+		fclose(pstream);
+	}
+
+	return 0;
+}
